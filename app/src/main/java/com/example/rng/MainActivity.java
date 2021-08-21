7acafbe4edaee51c855ad304d2eff923ec42a1e9
AmirRezaBehzad <amibehzad18@yahoo.com>
2021-08-22 00:36:34 +0430

    Half of UI is completed!

---
+++ app/src/main/java/com/example/rng/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.rng;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        getSupportActionBar().hide();
+    }
+}
