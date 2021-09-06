adc4ee18febcfee70ce7bbc9ac8cb80eeeb446fe
AmirRezaBehzad <amibehzad18@yahoo.com>
2021-09-06 15:21:08 +0430

    RNG is finished.I had to change its UI slightly though!

--- app/src/main/java/com/example/rng/MainActivity.java
+++ app/src/main/java/com/example/rng/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.rng;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,10 +27,9 @@ import static com.example.rng.R.id.nl_bottom_navigation;
 
 public class MainActivity extends AppCompatActivity {
 
-    Intent intent;
-    BottomNavigationView bottomNavigationView;
-    Button startbtn , resetbtn;
-    EditText editText , fromedittext , toedittext , mainedittext2;
+    Button startbtn , resetbtn , copybtn;
+    EditText fromedittext , toedittext;
+    TextView maintxtview , allnumberstxtview;
     Random rand = new Random();
     int from = 0 , to = 0, randnum = 0;
 
@@ -33,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
         fromedittext = findViewById(from_edittext);
         toedittext = findViewById(R.id.to_edittext);
-        editText = findViewById(R.id.main_edit_text);
-        editText.setInputType(InputType.TYPE_NULL);
+        maintxtview = findViewById(R.id.main_text_view);
+        allnumberstxtview = findViewById(R.id.all_numbers_text_view);
+        allnumberstxtview.setMovementMethod(new ScrollingMovementMethod());
 
         startbtn = findViewById(R.id.start_btn);
         startbtn.setOnClickListener(new View.OnClickListener() {
@@ -45,11 +50,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 if (fromedittext.getText().toString().equals("") == false && toedittext.getText().toString().equals("") == false) {
-                    editText.setText("");
-                    from = Integer.parseInt(fromedittext.getText().toString());
-                    to = Integer.parseInt(toedittext.getText().toString()) - 1;
-                    randnum = Integer.parseInt(String.valueOf(Math.round(Math.random() * (to - from + 1) + from)));
-                    editText.setText(String.valueOf(randnum));
+
+                    if (Integer.parseInt(fromedittext.getText().toString()) >= Integer.parseInt(toedittext.getText().toString()))
+                    {
+
+                    }
+                    else {
+                        maintxtview.setText("");
+                        from = Integer.parseInt(fromedittext.getText().toString());
+                        to = Integer.parseInt(toedittext.getText().toString()) - 1;
+                        randnum = Integer.parseInt(String.valueOf(Math.round(Math.random() * (to - from + 1) + from)));
+                        maintxtview.setText(String.valueOf(randnum));
+
+                        if (allnumberstxtview.getText().toString().equals(""))
+                            allnumberstxtview.setText(String.valueOf(randnum));
+
+                        else {
+                            String temp = allnumberstxtview.getText().toString();
+                            allnumberstxtview.setText(temp + " " + String.valueOf(randnum));
+                        }
+                    }
                 }
             }
         });
@@ -58,29 +78,29 @@ public class MainActivity extends AppCompatActivity {
         resetbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText("");
+                maintxtview.setText("");
                 fromedittext.setText("");
                 toedittext.setText("");
+                allnumberstxtview.setText("");
             }
         });
 
-        bottomNavigationView.setSelectedItemId(R.id.rng_bottom_navigation);
-
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        copybtn = findViewById(R.id.copy_button);
+        copybtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public void onClick(View view) {
+                if (allnumberstxtview.getText().toString().equals(""))
+                {
 
-                switch (item.getItemId()) {
-                    case R.id.rng_bottom_navigation:
-                        return true;
-
-                    case R.id.nl_bottom_navigation:
-                        startActivity(new Intent(getApplicationContext(), NumberListActivity1.class));
-                        overridePendingTransition(0, 0);
-                        return true;
                 }
-                return false;
+                else
+                {
+                    String temp = allnumberstxtview.getText().toString();
+                    ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    ClipData clipData = ClipData.newPlainText("Data" , temp);
+                    clipboardManager.setPrimaryClip(clipData);
+                    Toast.makeText(getApplicationContext(),"Copied to Clipboard",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
